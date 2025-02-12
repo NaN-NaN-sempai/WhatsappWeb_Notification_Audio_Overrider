2858a33dc299ad0e1f642dd1a901505740a4baf8
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2025-02-12 11:25:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-/* USING:
+
+/* 
+If you are using the installed whatsapp web chrome app, you can open it automatically on windows startup after server is created bt setting OPEN_WHATSAPP_CHROME_APP to true
+*/
+const OPEN_WHATSAPP_CHROME_APP = true;
+const OPEN_WHATSAPP_CHROME_APP_BAT_FILE_PATH = './openWhatsApp.bat';
+
+
+
+/* !!!! USING !!!!
 Allow CORS: Access-Control-Allow-Origin:
 https://chromewebstore.google.com/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf
 
@@ -27,8 +36,6 @@ to (same url from the express server):
 http://localhost:3000/
 */
 
-const OPEN_WHATSAPP_CHROME_APP = false;
-
 
 const express = require('express');
 const app = express();
@@ -36,28 +43,25 @@ const port = 3000;
 const { exec } = require('child_process');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/audio.mp3');
+	res.sendFile(__dirname + '/audio.mp3');
 });
 
 app.listen(port, () => {
-  console.log(`Audio Overrider in http://localhost:${port}`);
-  
-  /* 
-  If you are using the installed whatsapp web chrome app, you can open it automatically on windows startup after server is created bt setting OPEN_WHATSAPP_CHROME_APP to true
-  */
-  if(OPEN_WHATSAPP_CHROME_APP) {
-    const batFilePath = '<your path>\\whatsapp change notification sound\\openWhatsApp.bat';
-  
-    exec(`"${batFilePath}"`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-    });
-  }
+	console.log(`Audio Overrider in http://localhost:${port}`);
+
+	if(OPEN_WHATSAPP_CHROME_APP) {
+		const batFilePath = OPEN_WHATSAPP_CHROME_APP_BAT_FILE_PATH;
+
+		exec(`"${batFilePath}"`, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			return;
+		}
+		if (stderr) {
+			console.error(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+		});
+	}
 });
